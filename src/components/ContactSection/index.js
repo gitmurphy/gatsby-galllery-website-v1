95602e32fdff0ea5a24e276a5ab614ce38340502
gitmurphy <petermurphy5672@gmail.com>
2022-07-01 07:31:34 +0200

    updated contact form fields to include first and last name, textarea. Also included formsubmit email address for testing

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ContactSection.css" 
+import "./ContactSection.css"
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 function ContactSection() {
@@ -8,24 +8,44 @@ function ContactSection() {
             <Container>
                 <Row>
                     <Col lg={6} md={12}>
+
                         <h3>Contact Maeve</h3>
-                        <Form action="https://formsubmit.co/petermurphy5672@.com" method="POST">
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                        <Form action="https://formsubmit.co/petermurphy5672@gmail.com" method="POST">
+
+                            <Form.Group className="mb-3" controlId="formFirstName">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="text" placeholder="your first name" />
+                                <Form.Text className="text-muted">
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formLastName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="text" placeholder="your last name" />
+                                <Form.Text className="text-muted">
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control type="email" placeholder="your email address" />
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
                                 </Form.Text>
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                            <Form.Group className="mb-3" controlId="formMessage">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" rows={5} placeholder="your message"/>
                             </Form.Group>
+
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
+
                         </Form>
+
                     </Col>
                 </Row>
             </Container>
