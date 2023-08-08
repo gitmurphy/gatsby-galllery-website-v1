c29830a5902543242a3333ac70a3e85773c9911a
gitmurphy <petermurphy5672@gmail.com>
2023-08-08 22:13:20 +0200

    Let your light shine collection

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import HeroSection from "../components/HeroSection";
 import LAFGallery from "../components/LAFGallery";
+import LYLSGallery from "../components/LYLSGallery";
 
 const IndexPage = () => (
   <Layout>
       <Seo title="Home" />
       <HeroSection/>
       <LAFGallery/>
+      <LYLSGallery/>
   </Layout>
 )
 
